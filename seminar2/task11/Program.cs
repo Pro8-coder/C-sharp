47953df859057ba5999d1c366b9f5af91c0bf740
Pro8 <111078922+Pro8-coder@users.noreply.github.com>
2022-09-03 15:11:49 +0300

    Сделал task9, 11, 12, 14, 16

---
+++ seminar2/task11/Program.cs
@@ -0,0 +1,13 @@
+﻿/*11. Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+456 -> 46
+782 -> 72
+918 -> 98*/
+
+Random rand = new Random();
+int number = rand.Next(100, 1000);
+Console.WriteLine($"Сгенерировалось случайное число: {number}");
+int digit1 = number / 100;
+int digit2 = number % 10;
+//Console.Write($"Новое число: {digit1}{digit2}");
+int number2 = digit1 * 10 + digit2;
+Console.Write($"Новое число:" + number2);
---
+++ seminar2/task12/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Задача 12: Напишите программу, которая будет принимать на вход два числа и выводить, является ли первое число кратным второму. Если число 1 не кратно числу 2, то программа выводит остаток от деления.
+34, 5 -> не кратно, остаток 4
+16, 4 -> кратно*/
+
+Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number2 == 0)
+{
+    Console.WriteLine($"Нельзя делить на 0");
+}
+else
+{
+    if (number1 % number2 == 0)
+    {
+        Console.WriteLine($"Число {number1} является кратным числу {number2}");
+    }
+    else
+    {
+        Console.WriteLine($"Число {number1} не является кратным числу {number2}, остаток от деления {number1 % number2}");
+    }
+}
