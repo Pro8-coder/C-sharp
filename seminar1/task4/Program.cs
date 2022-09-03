c26c6c2fa5dfd7ea2bd7c59a93503416730161a7
Pro8 <111078922+Pro8-coder@users.noreply.github.com>
2022-09-03 11:51:25 +0300

    Сделал task4

--- seminar1/task4/Program.cs
+++ seminar1/task4/Program.cs
@@ -3,22 +3,19 @@
 44 5 78 -> 78
 22 3 9 -> 22*/
 
-Console.WriteLine("Введите число: ");
+Console.WriteLine("Введите первое число: ");
 int number1 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите число: ");
+Console.WriteLine("Введите второе число: ");
 int number2 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите число: ");
+Console.WriteLine("Введите третье число: ");
 int number3 = Convert.ToInt32(Console.ReadLine());
 
-if(number1 > number2)
-{
-    Console.Write("max = ");
-    Console.WriteLine(number1);
-}
-else
-{
-    Console.Write("max = ");
-    Console.WriteLine(number2);
-}
+int max = number1;
+
+if(number2 > max) max = number2;
+if(number3 > max) max = number3;
+
+Console.Write("max = ");
+Console.WriteLine(max);
