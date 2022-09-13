0c607a093860416faedac9d8349a962888af792b
Pro8 <111078922+Pro8-coder@users.noreply.github.com>
2022-09-14 02:50:19 +0300

    Сделал task27

--- seminar4/HWtask29/Program.cs
+++ seminar4/HWtask29/Program.cs
@@ -1,17 +1,22 @@
 ﻿/*Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. Данные вводятся с консоли пользователем*/
 
-// int[] arr = new int[8];
-// int n = arr.Length;
-// Random rnd = new Random();
-// for (int i = 0; i < n; i++)
-// {
-//     arr[i] = rnd.Next(0, 2);
-// }
-// Console.WriteLine(string.Join(", ", arr));
+int numberRead(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
 
-//foreach (int el in arr)
-// {
-//   Console.Write(el);
-// }
 
-//Console.WriteLine(string.Join(", ", arr));
+int[] ArrConsole(int size)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = numberRead($"Введите числовой элемент массива под индексом {i}: ");
+    }
+    return arr;
+}
+
+
+int[] array = ArrConsole(8);
+Console.WriteLine(string.Join(", ", array));
