e3908d7e74f754f0e6bb42d3ebc1a28636e8248e
Pro8 <111078922+Pro8-coder@users.noreply.github.com>
2022-09-13 20:22:42 +0300

    Сделал task23

---
+++ seminar4/HWtask29/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. Данные вводятся с консоли пользователем*/
+
+// int[] arr = new int[8];
+// int n = arr.Length;
+// Random rnd = new Random();
+// for (int i = 0; i < n; i++)
+// {
+//     arr[i] = rnd.Next(0, 2);
+// }
+// Console.WriteLine(string.Join(", ", arr));
+
+//foreach (int el in arr)
+// {
+//   Console.Write(el);
+// }
+
+Console.WriteLine(string.Join(", ", arr));
