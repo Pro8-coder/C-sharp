e4d5d92453e231d0d4131ab25e335f2421cbe1ec
Pro8 <111078922+Pro8-coder@users.noreply.github.com>
2022-09-16 22:53:52 +0300

    Сделал task29

---
+++ seminar6/task42/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+45 -> 101101
+3 -> 11
+2 -> 10*/
+
+int numberRead(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+void ConvertArray10to2(int number)
+{
+    if (number <= 0) return;
+    ConvertArray10to2(number / 2);
+    Console.Write(number % 2);
+}
+
+
+int num = numberRead("Введите число: ");
+ConvertArray10to2(num);
---
+++ seminar6/task44/Program.cs
@@ -0,0 +1,36 @@
+﻿/*Задача 44: Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+Если N = 5 -> 0 1 1 2 3
+Если N = 3 -> 0 1 1
+Если N = 7 -> 0 1 1 2 3 5 8*/
+
+int numberRead(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+int num = numberRead("Введите число больше 2: ");
+int[] Fibonachi(int number)
+{
+    int[] arrNew = new int[num];
+    arrNew[0] = 0;
+    arrNew[1] = 1;
+    for (int i = 0; i < number - 2; i++)
+    {
+        arrNew[i + 2] = arrNew[i] + arrNew[i + 1];
+    }
+    return arrNew;
+}
+
+
+if (num > 2)
+{
+    int[] array = Fibonachi(num);
+    Console.Write(string.Join(", ", array));
+}
+else
+{
+    Console.Write("Введенное число должно быть больше 2!");
+}
+
