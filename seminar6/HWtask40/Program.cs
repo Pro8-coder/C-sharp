7bc12a4ed62e146ca9f6f87dd2412a5e36bbca93
Pro8 <111078922+Pro8-coder@users.noreply.github.com>
2022-09-19 17:02:37 +0300

    Сделал task40 и 41

--- seminar6/HWtask40/Program.cs
+++ seminar6/HWtask40/Program.cs
@@ -1,4 +1,33 @@
 ﻿/*Задача 40: Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник с сторонами такой длины.
 Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон.*/
 
+int numberRead(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
 
+
+bool TriangleInequalityTheorem(int number1, int number2, int number3)
+{
+    bool theorem = false;
+    if (number1 + number2 > number3 && number1 + number3 > number2 && number2 + number3 > number1)
+    {
+        theorem = true;
+    }
+    return theorem;
+}
+
+
+int num1 = numberRead("Введите первое число: ");
+int num2 = numberRead("Введите второе число: ");
+int num3 = numberRead("Введите третье число: ");
+bool triang = TriangleInequalityTheorem(num1, num2, num3);
+if (triang == true)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
