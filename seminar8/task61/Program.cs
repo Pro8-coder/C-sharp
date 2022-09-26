a6eb4ad1b3ac974ed03ac9e48574f08985a54341
Pro8 <111078922+Pro8-coder@users.noreply.github.com>
2022-09-26 13:31:16 +0300

    Сделал task47, 50, 52

--- seminar8/task61/Program.cs
+++ seminar8/task61/Program.cs
@@ -1,5 +1,55 @@
 ﻿/*Задача 61: Вывести первые N строк треугольника Паскаля. Сделать вывод в виде равнобедренного треугольника*/
 
+int numberRead(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
 
 
+int[,] GetMatrix(int rowsCount, int columnsCount, int leftRange = -10, int ringhtRange = 10)
+{
+    int[,] matrix = new int[rowsCount, columnsCount];
+    Random rand = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(leftRange, ringhtRange);
+        }
+    }
+    return matrix;
+}
 
+
+void PrintMatrix(int[,] matrix, string text = "")
+{
+    Console.WriteLine(text);
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(String.Format("{0,5:f1}", $"{matrix[i, j]} "));
+        }
+        Console.WriteLine();
+    }
+}
+
+
+void Pascal(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(String.Format("{0,5:f1}", $"{matrix[i, j]} "));
+        }
+        Console.WriteLine();
+    }
+}
+
+
+int m = numberRead("Введите количество строк");
+int n = numberRead("Введите количество столбцов");
+int[,] matr = GetMatrix(m, n);
+PrintMatrix(matr, "Рандомный массив");
