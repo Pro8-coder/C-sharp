41f93377eec51ba60455396be3d521875e061427
Pro8 <111078922+Pro8-coder@users.noreply.github.com>
2022-09-30 11:32:45 +0300

    Сделал task63

---
+++ seminar9/task63/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Задача 63: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+N = 5 -> "1, 2, 3, 4, 5"
+N = 6 -> "1, 2, 3, 4, 5, 6"*/
+
+int numberRead(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+void Print1toNRecurs(int n)
+{
+    if (n<=0)
+    {
+        return;
+    }
+    else
+    {
+        Print1toNRecurs(n-1);
+        Console.Write($"{n} ");
+    }
+}
+
+
+int N = numberRead("Введите число: ");
+Print1toNRecurs(N);
---
+++ seminar9/task65/Program.cs
@@ -0,0 +1,34 @@
+﻿/*Задача 65: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+M = 1; N = 5 -> "1, 2, 3, 4, 5"
+M = 4; N = 8 -> "4, 5, 6, 7, 8"*/
+
+int numberRead(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+void PrintMtoNRecurs(int m, int n)
+{
+    if (m > n)
+    {
+        int temp = n;
+        n = m;
+        m = temp;
+    }
+    if (n >= m)
+    {
+        return;
+    }
+    else
+    {
+        Print1toNRecurs(m + 1);
+        Console.Write($"{m} ");
+    }
+}
+
+
+int M = numberRead("Введите число M: ");
+int N = numberRead("Введите число N: ");
+PrintMtoNRecurs(M, N);
